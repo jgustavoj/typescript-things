bf81a23e3c2722b6d7f09283da1abb755b58d156
jgustavoj <gjime011@fiu.edu>
2022-06-06 13:44:43 -0400

    literal types p-2

--- app.ts
+++ app.ts
@@ -21,15 +21,24 @@
 function combine(
   input1: number | string,
   input2: number | string,
-  resultConversion: string
+  resultConversion: "as-number" | "as-text"
 ) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+  //   if (resultConversion === "as-number") {
+  //     // the + in front turns it into a number, you can also use parseFloat()
+  //     return +result;
+  //   } else {
+  //     return result.toString();
+  //   }
 }
 
 const combineAges = combine(30, 26, "as-number");
